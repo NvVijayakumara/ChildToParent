cf031ddee30778d4af2c78666ac6b05f60a9c15f
Vijay.nv <Vijay.nv@VSPLBLRLT0231>
2021-01-21 20:47:56 +0530

    Add project files.

---
+++ ChildToParent/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChildToParent
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string City { get; set; }
+    }
+}
